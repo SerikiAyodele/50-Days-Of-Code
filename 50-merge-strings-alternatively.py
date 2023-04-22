384200987574936e4e9395d47531ca782bb23b1d
Ayodele Seriki <serikiayodele@gmail.com>
2023-04-22 16:03:33 +0100

    day 50

---
+++ 50-merge-strings-alternatively.py
@@ -0,0 +1,15 @@
+def mergeStrings(word1: str, word2: str) -> str:
+    ptr1, ptr2 = 0, 0
+    merged = ""
+
+    while ptr1 < len(word1) and ptr2 < len(word2):
+        merged += word1[ptr1] + word2[ptr2]
+        ptr1 += 1
+        ptr2 += 1
+
+    if len(word1) > len(word2):
+        merged += word1[ptr1:]
+    elif len(word2) > len(word1):
+        merged += word1[ptr2:]
+
+    return merged
